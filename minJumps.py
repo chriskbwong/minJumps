82fb1a9223715ecd5987e67aba7bc247826d8cc7
Chris Wong <chriskbwong@gmail.com>
2019-01-18 22:23:04 -0800

    Create minJumps.py

---
+++ minJumps.py
@@ -0,0 +1,28 @@
+# Returns minimum number of jumps 
+# to reach arr[n-1] from arr[0] 
+def minJumps(arr, n): 
+    jumps = [0 for i in range(n)] 
+  
+    if (n == 0) or (arr[0] == 0): 
+        return float('inf') 
+  
+    jumps[0] = 0
+  
+    # Find the minimum number of  
+    # jumps to reach arr[i] from  
+    # arr[0] and assign this  
+    # value to jumps[i] 
+    for i in range(1, n): 
+        jumps[i] = float('inf') 
+        for j in range(i): 
+            if (i <= j + arr[j]) and (jumps[j] != float('inf')): 
+                jumps[i] = min(jumps[i], jumps[j] + 1) 
+                break
+    return jumps[n-1] 
+  
+# Driver Program to test above function 
+arr = [4, 2, 0, 0, 2, 0] 
+size = len(arr) 
+print('Minimum number of jumps to reach', 
+      'end is', minJumps(arr,size)) 
+  
